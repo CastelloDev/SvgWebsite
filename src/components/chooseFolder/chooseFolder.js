d2f647b10d57ac79db2ee5dd5fb9576bf23dd495
Andileh <amathenjwa@retrorabbit.co.za>
2019-09-09 16:12:31 +0200

    removing white spaces

--- src/components/chooseFolder/chooseFolder.js
+++ src/components/chooseFolder/chooseFolder.js
@@ -1,19 +1,16 @@
 import React, { Component } from "react";
 class chooseFolder extends Component {
     onChangeHandler=event=>{
-      console.log(event.target.files);//give you the path of te files iside the folder
-
+      console.log(event.target.files);
     this.setState({
       selectedFile: event.target.files[0],
       loaded: 0,
     })
-    
-}
+  }
 
   render() {
-  
     return (
-      <div className="">
+      <div className="upload">
         <input directory=""  webkitdirectory="" type="file" onChange={this.onChangeHandler} />
 
        </div>
