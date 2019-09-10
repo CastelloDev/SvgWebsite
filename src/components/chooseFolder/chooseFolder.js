3f3d2b493cc4c08821183b101e5194fba194cb18
Andileh <amathenjwa@retrorabbit.co.za>
2019-09-10 15:29:20 +0200

    get names of the files

--- src/components/chooseFolder/chooseFolder.js
+++ src/components/chooseFolder/chooseFolder.js
@@ -1,29 +1,68 @@
 import React, { Component } from "react";
 class chooseFolder extends Component {
-  constructor(props){
-    super(props)
+
+  constructor(props) {
+    super(props);
     this.state={
-      path:[]
+      path:[],
+      arr:null,
+       displayMenu: false,
+       count:0
 
     }
+ 
+    this.showDropdownMenu = this.showDropdownMenu.bind(this);
+    this.hideDropdownMenu = this.hideDropdownMenu.bind(this);
+   
+  }
+  showDropdownMenu(event) {
+    event.preventDefault();
+    this.setState({ displayMenu: true }, () => {
+    document.addEventListener('click', this.hideDropdownMenu);
+    });
   }
 
+  hideDropdownMenu() {
+    this.setState({ displayMenu: true }, () => {
+      document.removeEventListener('click', this.hideDropdownMenu);
+    });
+
+  }
   onChangeHandler=event=>{
      this.setState({path:event.target.files})
   }
-  
+
   render() {
-    for(var item of this.state.path) {
-      console.log(item);
-    }
+   var arr = [];
+   for (var key in this.state.path) {
+      if(key<this.state.path.length){
+      arr.push(this.state.path[key]);
+  }
   
+}
+   const listItems = arr.map((item,key) =>
+    <li key={key}>{item.name}</li>
+     );
     return (
-      <div>
-        <input  webkitdirectory="" type="file" onChange={this.onChangeHandler} />
-      </div>
-     
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          <input type="file"   webkitdirectory=""   onChange={this.onChangeHandler} />
+        </label>
+        <br />
+         <br />  
+         <button className="button" onClick={this.showDropdownMenu}> Files to optimize </button>
+           { this.state.displayMenu ? (
+          <ul>
+            {listItems}
+          </ul>
+        ):
+        (
+          null
+        )
+        }
+        <br />
+      </form>
     );
   }
 }
-
 export default chooseFolder;
