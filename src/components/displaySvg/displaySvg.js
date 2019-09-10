5777b60f6f654b15e92fcce3593714dc6041efd8
sjumba@retrorabbit.co.za <sjumba@retrorabbit.co.za>
2019-09-10 08:25:58 +0200

    formated

--- src/components/displaySvg/displaySvg.js
+++ src/components/displaySvg/displaySvg.js
@@ -1,16 +1,15 @@
 import React, { Component } from "react";
 
-
 class DisplaySvg extends Component {
   render() {
     return (
       <div className="App">
-            <div className="svg-item">
-              <img
-                src={this.props.dataUrl}
-                width={this.props.width}
-                height={this.props.height}
-              />
+        <div className="svg-item">
+          <img
+            src={this.props.dataUrl}
+            width={this.props.width}
+            height={this.props.height}
+          />
         </div>
       </div>
     );
