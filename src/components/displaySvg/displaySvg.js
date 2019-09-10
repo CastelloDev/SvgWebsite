4a0e0d39a5e7bf040134c059a97a99911e5afbbb
sjumba@retrorabbit.co.za <sjumba@retrorabbit.co.za>
2019-09-10 08:25:05 +0200

    removed unused divs

--- src/components/displaySvg/displaySvg.js
+++ src/components/displaySvg/displaySvg.js
@@ -5,19 +5,12 @@ class DisplaySvg extends Component {
   render() {
     return (
       <div className="App">
-        <div className="Buttons-div">
-          <div id="display-svg">
             <div className="svg-item">
-              <div>
               <img
                 src={this.props.dataUrl}
                 width={this.props.width}
                 height={this.props.height}
               />
-              </div>
-            </div>
-            <div></div>
-          </div>
         </div>
       </div>
     );
