f18d423d7bda89adb75426548bc3982a7d584dc9
sjumba@retrorabbit.co.za <sjumba@retrorabbit.co.za>
2019-09-10 15:51:24 +0200

    Removed extra div

--- src/components/displaySvg/displaySvg.js
+++ src/components/displaySvg/displaySvg.js
@@ -4,13 +4,11 @@ class DisplaySvg extends Component {
   render() {
     return (
       <div>
-        <div>
           <img
             src={this.props.dataUrl}
             width={this.props.width}
             height={this.props.height}
           />
-        </div>
       </div>
     );
   }
