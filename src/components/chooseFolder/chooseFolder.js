9909f9c314644901e3d51fec19ff4e75af7e9e67
sjumba@retrorabbit.co.za <sjumba@retrorabbit.co.za>
2019-09-10 15:04:42 +0200

    deleted master files.

--- src/components/chooseFolder/chooseFolder.js
+++ src/components/chooseFolder/chooseFolder.js
@@ -1,29 +1,26 @@
 import React, { Component } from "react";
 class chooseFolder extends Component {
-  constructor(props){
-    super(props)
-    this.state={
-      path:[]
-
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      path: []
+    };
   }
 
-  onChangeHandler=event=>{
-     this.setState({path:event.target.files})
-  }
-  
+  onChangeHandler = event => {
+    this.setState({ path: event.target.files });
+  };
+
   render() {
-    for(var item of this.state.path) {
+    for (var item of this.state.path) {
       console.log(item);
     }
-  
+
     return (
       <div>
-        <input  webkitdirectory="" type="file" onChange={this.onChangeHandler} />
+        <input webkitdirectory="" type="file" onChange={this.onChangeHandler} />
       </div>
-     
     );
   }
 }
-
 export default chooseFolder;
