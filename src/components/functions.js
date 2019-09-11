fe2268ce53a3da8e21c2e2cea1230abe95a53e09
Mpilo Mshengu <mmshengu@retrorebbit.co.za>
2019-09-11 12:04:21 +0200

    changes

--- src/components/functions.js
+++ src/components/functions.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import {BASE_URL,OPTIMIZE_SVG} from '../constants/constants';
+import axios from "axios";
+import {BASE_URL,OPTIMIZE_SVG} from "../constants/constants";
 
 export default  function optimizeSvg(dataUrl){
   return axios.post(`${BASE_URL}${OPTIMIZE_SVG}`, { dataUrl })
