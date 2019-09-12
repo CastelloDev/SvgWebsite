9b3a6bc9fe0bbc6b158d053064ff5d1786bf4c24
sjumba@retrorabbit.co.za <sjumba@retrorabbit.co.za>
2019-09-12 14:20:11 +0200

    Installed axios package after pulling

--- src/components/chooseFolder/chooseFolder.js
+++ src/components/chooseFolder/chooseFolder.js
@@ -1,72 +1,81 @@
-import React, { Component } from 'react';
-import './../checkBoxSelection/checkBoxSelection.scss';
+import React, { Component } from "react";
+import "./../checkBoxSelection/checkBoxSelection.scss";
 import optimizeSvg from "../functions";
 
-import CheckBoxSelection from '../checkBoxSelection/checkBoxSelection';
+import CheckBoxSelection from "../checkBoxSelection/checkBoxSelection";
 class chooseFolder extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			path: [],
-			displayMenu: false,
-      originalDataUrl:"",
-      optimizedDataUrl:""
-    }
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      path: [],
+      displayMenu: false,
+      originalDataUrl: "",
+      optimizedDataUrl: ""
+    };
+  }
 
-	showDropdownMenu = event => {
-		event.preventDefault();
-		this.setState({ displayMenu: true }, () => {
-			document.addEventListener('click', this.hideDropdownMenu);
-		});
-	};
-
-	hideDropdownMenu = () => {
-		this.setState({ displayMenu: true }, () => {
-			document.removeEventListener('click', this.hideDropdownMenu);
-		});
+  showDropdownMenu = event => {
+    event.preventDefault();
+    this.setState({ displayMenu: true }, () => {
+      document.addEventListener("click", this.hideDropdownMenu);
+    });
   };
-  
-	onChangeHandler = event => {
-     this.setState({path:event.target.files});
-  }
 
-  convertSvgToDataUrl=(file)=>{
-    let reader  = new FileReader();
-      reader.onload = (result)=>{
-        this.setState({originalDataUrl:result.target.result});
-      }
-      if (file) {
-        reader.readAsDataURL(file);
-      }
-  }
+  hideDropdownMenu = () => {
+    this.setState({ displayMenu: true }, () => {
+      document.removeEventListener("click", this.hideDropdownMenu);
+    });
+  };
 
-   optimizeSvgDataUrl= async (dataUrl) =>{
-      this.setState({optimizedDataUrl:await optimizeSvg(dataUrl)});
-  }
+  onChangeHandler = event => {
+    this.setState({ path: event.target.files });
+  };
+
+  convertSvgToDataUrl = file => {
+    let reader = new FileReader();
+    reader.onload = result => {
+      this.setState({ originalDataUrl: result.target.result });
+    };
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+
+  optimizeSvgDataUrl = async dataUrl => {
+    this.setState({ optimizedDataUrl: await optimizeSvg(dataUrl) });
+  };
 
   render() {
-		
-		const listOfFileNames = [];
-		for(var key of this.state.path) {
-			listOfFileNames.push(<CheckBoxSelection key={key.name} filenames={key.name} />);
-		}
-	
-  return (
-			<div className="main">
-				<div className="myfiles">
-					<label>
-						<input type="file" webkitdirectory="" onChange={this.onChangeHandler} />
-					</label>
-				</div>
-				<div className="myfiles">
-					<button onClick={this.showDropdownMenu}> Files to optimize </button>
-				</div>
-				<div className={this.state.displayMenu ? "show-file-list":"hide-file-list"}>
-					{listOfFileNames}
-				</div>
-			</div>
-		);
-	} 
+    const listOfFileNames = [];
+    for (var key of this.state.path) {
+      listOfFileNames.push(
+        <CheckBoxSelection key={key.name} filenames={key.name} />
+      );
+    }
+
+    return (
+      <div className="main">
+        <div className="myfiles">
+          <label>
+            <input
+              type="file"
+              webkitdirectory=""
+              onChange={this.onChangeHandler}
+            />
+          </label>
+        </div>
+        <div className="myfiles">
+          <button onClick={this.showDropdownMenu}> Files to optimize </button>
+        </div>
+        <div
+          className={
+            this.state.displayMenu ? "show-file-list" : "hide-file-list"
+          }
+        >
+          {listOfFileNames}
+        </div>
+      </div>
+    );
+  }
 }
 export default chooseFolder;
