26d9d99ed86b15f08e85fa1e8629cb5e30b91d76
sjumba@retrorabbit.co.za <sjumba@retrorabbit.co.za>
2019-09-12 14:12:51 +0200

    Calculate file size from DataUrl. and display it underneath an image.

--- src/components/displaySvg/displaySvg.js
+++ src/components/displaySvg/displaySvg.js
@@ -1,14 +1,43 @@
 import React, { Component } from "react";
+import base64 from "base-64";
 
 class DisplaySvg extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fileSize: 0
+    };
+    this.dataURLtoKilloBytes = this.dataURLtoKilloBytes.bind(this);
+  }
+
+  dataURLtoKilloBytes = dataURI => {
+    return base64.decode(dataURI).length / 1000;
+  };
+
+  componentWillMount = () => {
+    console.log();
+    if (this.props.dataUrl) {
+      this.setState({
+        fileSize:
+          this.dataURLtoKilloBytes(
+            this.props.dataUrl.split(",")[1]
+          ).toString() + " KB"
+      });
+    }
+  };
   render() {
     return (
       <div>
+        <figure>
           <img
+            id="svg-display-id"
             src={this.props.dataUrl}
             width={this.props.width}
             height={this.props.height}
+            alt="svg image"
           />
+          <figcaption>{"File Size : " + this.state.fileSize}</figcaption>
+        </figure>
       </div>
     );
   }
