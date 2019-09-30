5741ff1ef2eb53004489186caf5ca4a56388066e
sjumba@retrorabbit.co.za <sjumba@retrorabbit.co.za>
2019-09-30 17:35:17 +0200

    renamed a function

--- src/components/checkBoxSelection/checkBoxSelection.js
+++ src/components/checkBoxSelection/checkBoxSelection.js
@@ -1,85 +1,108 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import './checkBoxSelection.scss';
-import { connect } from 'react-redux';
-import { ADD_VARIABLE, UPDATE_VARIABLE, DELETE_VARIABLE } from '../../store/actionTypes';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./checkBoxSelection.scss";
+import { connect } from "react-redux";
+import {
+  ADD_VARIABLE,
+  UPDATE_VARIABLE,
+  DELETE_VARIABLE
+} from "../../store/actionTypes";
 
 class CheckBoxSelection extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-			 isClicked: this.props.isSelected
-		 };
+  constructor(props) {
+    super(props);
+    this.state = {
+      isClicked: this.props.isSelected
+    };
+  }
+
+  convertSvgToDataUrl = file => {
+    let reader = new FileReader();
+    reader.onload = result => {
+      this.props.reduxState.svgSettingList.push({
+        name: file.name,
+        dataurl: result.target.result
+      });
+    };
+    if (file) {
+      reader.readAsDataURL(file);
     }
+  };
 
-	convertSvgToDataUrl=(file)=>{
-		 let reader  = new FileReader();
-		 reader.onload = (result)=>{
-			 this.props.reduxState.svgSettingList.push ({'name':file.name,dataurl:result.target.result});
-		 };
-		 if (file) {
-		 	reader.readAsDataURL(file);
-		 }
-	}
+  changeColourAndTick = () => {
+    let isFound = false;
+    let index = 0;
+    this.setState({ isClicked: !this.state.isClicked });
+    for (var key in this.props.reduxState.svgSettingList) {
+      if (
+        this.props.reduxState.svgSettingList[key].name === this.props.file.name
+      ) {
+        isFound = true;
+        index = key;
+      }
+    }
+    if (!isFound) {
+      this.convertSvgToDataUrl(this.props.file);
+    } else {
+      this.props.reduxState.svgSettingList.splice(index, 1);
+      isFound = false;
+    }
+  };
 
-	handleDivClick= () => {
-		 let isFound=false;
-		 let index=0;
-	     this.setState({isClicked: !this.state.isClicked});
-		 for(var key in this.props.reduxState.svgSettingList){
-			 if(this.props.reduxState.svgSettingList[key].name===this.props.file.name){
-				 isFound=true;
-				 index=key;
-			 }
-		 }
-		 if(!isFound){
-			 this.convertSvgToDataUrl(this.props.file);	
-		 }else{
-			 this.props.reduxState.svgSettingList.splice(index, 1);
-			 isFound=false;
-		 }
-	}
-
-	render() {
-
-	    return (
-	        <div
-    		className={this.state.isClicked ? 'checkbox-container-clicked' : 'checkbox-container-unclicked'}
-    		onClick={this.handleDivClick}
-	        >
-	            <input
-    			className='checkbox-input'
-    			type='checkbox'
-    			checked={this.state.isClicked}
-	            />
-	            <label className='checkbox-label'>
-					  {this.props.filename}
-						
-	            </label>
-	        </div>
-	    );
-	}
+  render() {
+    return (
+      <div
+        className={
+          this.state.isClicked
+            ? "checkbox-container-clicked"
+            : "checkbox-container-unclicked"
+        }
+        onClick={this.changeColourAndTick}
+      >
+        <input
+          className="checkbox-input"
+          type="checkbox"
+          checked={this.state.isClicked}
+        />
+        <label className="checkbox-label">{this.props.filename}</label>
+      </div>
+    );
+  }
 }
 
-CheckBoxSelection.propTypes={
-    reduxState: PropTypes.object,
-    addToStore: PropTypes.func,
-    updateStore: PropTypes.func,
-	 deleteFromStore: PropTypes.func,
-	 filename:PropTypes.string
+CheckBoxSelection.propTypes = {
+  reduxState: PropTypes.object,
+  addToStore: PropTypes.func,
+  updateStore: PropTypes.func,
+  deleteFromStore: PropTypes.func,
+  filename: PropTypes.string
 };
 
 const mapStateToProps = state => {
-    const reduxState = {reduxState:{...state}};
-    return reduxState;
+  const reduxState = { reduxState: { ...state } };
+  return reduxState;
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-        addToStore: (variableName, variableValue) => dispatch({ type: ADD_VARIABLE, variableName: variableName, variableValue: variableValue }),
-        updateStore: (variableName, variableValue) => dispatch({ type: UPDATE_VARIABLE, variableName: variableName, variableValue: variableValue }),
-        deleteFromStore: (variableName) => dispatch({ type: DELETE_VARIABLE, variableName: variableName })
-    };
+  return {
+    addToStore: (variableName, variableValue) =>
+      dispatch({
+        type: ADD_VARIABLE,
+        variableName: variableName,
+        variableValue: variableValue
+      }),
+    updateStore: (variableName, variableValue) =>
+      dispatch({
+        type: UPDATE_VARIABLE,
+        variableName: variableName,
+        variableValue: variableValue
+      }),
+    deleteFromStore: variableName =>
+      dispatch({ type: DELETE_VARIABLE, variableName: variableName })
+  };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(CheckBoxSelection);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CheckBoxSelection);
--- src/components/svgSetting/svgSettingOptions.js
+++ src/components/svgSetting/svgSettingOptions.js
@@ -1,35 +1,43 @@
-import React, { Component } from 'react';
-import './../checkBoxSelection/checkBoxSelection.scss';
-import {CardLayout} from './svgCardLayout';
-import SvgSetting from './svgSetting';
-import Slider from './slider';
+import React, { Component } from "react";
+import "./../checkBoxSelection/checkBoxSelection.scss";
+import { CardLayout } from "./svgCardLayout";
+import SvgSetting from "./svgSetting";
+import Slider from "./slider";
 class SvgSettingOptions extends Component {
- 	constructor(props) {
-	 	super(props);
-	 	this.state = {
-	 	settingOptionList : []
-        };
-	 }
+  constructor(props) {
+    super(props);
+    this.state = {
+      settingOptionList: []
+    };
+  }
 
-    render() {
-    	for(var key in CardLayout) {
-		  if(CardLayout[key].type==='checkbox'){
-					 this.state.settingOptionList.push(<SvgSetting key={key} value={CardLayout[key].value} option={CardLayout[key].text} />);
-		  	}else if(CardLayout[key].type==='slider'){
-					 this.state.settingOptionList.push(<div className='slider'><Slider key={key} option={CardLayout[key].text} /></div>);
-	 		}
-	 	}
-	 
- 	return (
-	 		<div className='card-settings' >
-	            <div >
-			 		<label> {'SVG settings'} </label>
-		 		</div>
-		 		<div >
-		 			{ this.state.settingOptionList}
-		 		</div>
-		 	</div>
-	 	);
- 	}
-} 
+  render() {
+    for (var key in CardLayout) {
+      if (CardLayout[key].type === "checkbox") {
+        this.state.settingOptionList.push(
+          <SvgSetting
+            key={key}
+            value={CardLayout[key].value}
+            option={CardLayout[key].text}
+          />
+        );
+      } else if (CardLayout[key].type === "slider") {
+        this.state.settingOptionList.push(
+          <div className="slider">
+            <Slider key={key} option={CardLayout[key].text} />
+          </div>
+        );
+      }
+    }
+
+    return (
+      <div className="card-settings">
+        <div>
+          <label> {"SVG settings"} </label>
+        </div>
+        <div>{this.state.settingOptionList}</div>
+      </div>
+    );
+  }
+}
 export default SvgSettingOptions;
