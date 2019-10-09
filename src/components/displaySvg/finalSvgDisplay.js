339e41325e77b1e1ec3b38ddceb3a320d23c8711
sjumba@retrorabbit.co.za <sjumba@retrorabbit.co.za>
2019-10-09 13:54:44 +0200

    Ïts hovering some parts with minor errors

--- src/components/displaySvg/finalSvgDisplay.js
+++ src/components/displaySvg/finalSvgDisplay.js
@@ -5,6 +5,7 @@ import finalSvgDisplay from "./finalSvgDisplay.scss";
 import { connect } from "react-redux";
 import base64 from "base-64";
 import InlineSVG from "svg-inline-react";
+import {SVG_TAG_NAMES} from "../../constants/constants";
 import {
   ADD_VARIABLE,
   UPDATE_VARIABLE,
@@ -21,6 +22,10 @@ class FinalSvgDisplay extends Component {
     };
 }
 
+svgPathClick(){
+  alert("I have been clicked");
+}
+
 
 
   componentDidMount = () => {
@@ -29,17 +34,26 @@ class FinalSvgDisplay extends Component {
     var pathArray = document.getElementsByTagName("path");
     console.log("pathArray : ", pathArray);
     console.log("pathArray : ", pathArray.length);
+    
 
-    for (var i = 0; i < pathArray.length; i++) {
+    for (var x of pathArray) {
+      console.log("X------> : ",x);
+    }
+
+    var i = 0;
+    for (var x of pathArray) {
       var elementWrapper = document.createElement("a");
       elementWrapper.setAttribute("class","className-1");
+      elementWrapper.setAttribute("onClick",this.svgPathClick);
       elementWrapper.setAttribute("id","id-1");
-      console.log("pathArray["+ i +"]",pathArray[i]);
-      pathArrayLocal.push(pathArray[i]);
-      elementWrapper.appendChild(pathArray[i]);
+      console.log("element ["+ i +"]",x);
+      pathArrayLocal.push(x);
+      elementWrapper.appendChild(x);
       array.push(elementWrapper);
-
+      console.log("elementWrapper  :",`${i } elementWrapper`);
+      i++;
     }
+
 
     this.setState({
       wrappedPathsElement : array,
@@ -57,8 +71,15 @@ class FinalSvgDisplay extends Component {
       var doc = new DOMParser().parseFromString(stringElement, "text/html").body.firstChild;
       if(this.state.wrappedPathsElement.length > 0){
         for(var index=0; index < this.state.wrappedPathsElement.length ; index++){
-            var tempStr = new XMLSerializer().serializeToString(this.state.wrappedPathsElement[index]).toString();
-            stringElement = stringElement.replace();
+            var tempWrapedStr = new XMLSerializer().serializeToString(this.state.wrappedPathsElement[index]).toString();
+            var tempToWrapStr = new XMLSerializer().serializeToString(this.state.pathArrayState[index]).toString();
+            tempToWrapStr =  tempToWrapStr.replace("xmlns=\"http://www.w3.org/2000/svg\"","");
+            tempToWrapStr = tempToWrapStr.substring(0,5)+" "+tempToWrapStr.substring(6,tempToWrapStr.length).trim();
+            // console.log("replace ----> "+tempToWrapStr.substring(0,5)+" "+tempToWrapStr.substring(6,tempToWrapStr.length).trim()+ "with this ---> "+tempWrapedStr);
+            // console.log("tempToWrapStr : ", tempToWrapStr.substring(41,tempToWrapStr.length));
+            // console.log("tempWrapedStr) : ", tempWrapedStr.substring(41,tempWrapedStr.length));
+            stringElement = stringElement.replace(tempToWrapStr, tempWrapedStr);
+            console.log("stringElement : ", stringElement);
         }
       }
 
